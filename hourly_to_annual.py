5d06c469dcfcf754cf182fcfffc2dddd151b06ed
Jane Nevins <jane.e.nevins@gmail.com>
2020-09-15 14:51:46 -0400

    Update hourly_to_annual.py

--- hourly_to_annual.py
+++ hourly_to_annual.py
@@ -1,5 +1,8 @@
 def hourly(x):
     """enter hourly salary and return annual salary"""
+    ann_salary = x * 40.0 * 52.0 #assumes 40 hours a week and 52 weeks in a year
+    print(f"The anual salary is ${ann_salary:,.2f}")
 
-    ann_salary = x * 40 * 52 #assumes 40 hours a week and 52 weeks in a year
-    return ann_salary
+x = float(input("Enter the hourly salary in dollars.  "))
+
+hourly(x)
